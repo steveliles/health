8baaf2db5405ee557c027cb336461ce95ceaaf9d
Steve Liles <steveliles@gmail.com>
2013-02-19 09:20:23 +0000

    factoring out common code

--- src/main/java/com/sjl/health/internal/SimpleThreadSafeHealth.java
+++ src/main/java/com/sjl/health/internal/SimpleThreadSafeHealth.java
@@ -15,12 +15,26 @@ public class SimpleThreadSafeHealth implements Health {
 	private State state;
 	private HistoryManager history;
 	private HealthListeners listeners;
+	private StateInvoker success;
+	private StateInvoker failure;
 	
 	public SimpleThreadSafeHealth(State anInitialState, HistoryManager aHistoryManager) {
 		state = anInitialState;
 		lock = new Object();
 		listeners = new HealthListeners();
 		history = aHistoryManager;
+		
+		success = new StateInvoker() {
+			protected State invokeImpl(Throwable aMaybe) {
+				return state.success();
+			}			
+		};
+		
+		failure = new StateInvoker() {
+			protected State invokeImpl(Throwable aMaybe) {
+				return state.failure(aMaybe);
+			}
+		};
 	}
 	
 	@Override
@@ -50,42 +64,35 @@ public class SimpleThreadSafeHealth implements Health {
 
 	@Override
 	public void success() {
-		StateInfo _before, _after = null;
-		
-		synchronized(lock) {
-			_before = state;
-			state = state.success();
-			_after = state;
-			
-			if (_before != _after) {
-				history.add(_after);
-				_before = ImmutableStateInfo.create(_before);
-				_after = ImmutableStateInfo.create(_after);
-			}
-		}
-		
-		if (!_before.equals(_after))
-			listeners.onChange(_before, _after);
+		success.invoke(null);
 	}
 
 	@Override
 	public void failure(Throwable aCause) {
-		StateInfo _before, _after = null;
-		
-		synchronized(lock) {
-			_before = state;
-			state = state.failure(aCause);
-			_after = state;
-			
-			if (_before != _after) {
-				history.add(_after);
-				_before = ImmutableStateInfo.create(_before);
-				_after = ImmutableStateInfo.create(_after);
-			}
-		}
-		
-		if (!_before.equals(_after))
-			listeners.onChange(_before, _after);
+		failure.invoke(aCause);
 	}
-
+	
+	abstract class StateInvoker {
+		
+		protected abstract State invokeImpl(Throwable aMaybe);
+		
+		public void invoke(Throwable aMaybe) {
+			StateInfo _before, _after = null;
+			
+			synchronized(lock) {
+				_before = state;
+				state = invokeImpl(aMaybe);
+				_after = state;
+				
+				if (_before != _after) {
+					history.add(_after);
+					_before = ImmutableStateInfo.create(_before);
+					_after = ImmutableStateInfo.create(_after);
+				}
+			}
+			
+			if (!_before.equals(_after))
+				listeners.onChange(_before, _after);
+		}
+	}
 }
